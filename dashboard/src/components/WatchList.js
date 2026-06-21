bca8d4d73e63b0b8e57ab64bfc89a8f8d9d68b8e
Kamaljeet Kaur <168989813+KK15082005@users.noreply.github.com>
2026-06-21 15:55:27 +0530

    Add WatchList and WatchListItem components

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,6 @@
 import React, { useState, useContext } from "react";
+import {watchlist} from "../data/data" ;
+
 
 const WatchList = () => {
   return (
@@ -11,13 +13,79 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
       <ul className="list">
+        {watchlist.map((stock , index)=>{
+          
+          return(
+            <WatchListItem stock={stock} key={index}/>
+          )
+          
+        })}
       </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+  return (
+    <li
+      onMouseEnter={() => setShowWatchlistActions(true)}
+      onMouseLeave={() => setShowWatchlistActions(false)}
+    >
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>
+          {stock.name}
+        </p>
+
+        <div className="item-info">
+          <span className="percent">{stock.percent}</span>
+
+          {stock.isDown ? (
+            <i className="bi bi-caret-down-fill down"></i>
+          ) : (
+            <i className="bi bi-caret-up-fill up"></i>
+          )}
+
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+
+      {showWatchlistActions && (
+        <WatchListActions uid={stock.name} />
+      )}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <div className="actions">
+      <span>
+        <button className="buy">Buy</button>
+      </span>
+
+      <span>
+        <button className="sell">Sell</button>
+      </span>
+
+      <span>
+        <button className="action">
+          <i className="bi bi-bar-chart"></i>
+        </button>
+      </span>
+
+      <span>
+        <button className="action">
+          <i className="bi bi-three-dots"></i>
+        </button>
+      </span>
+    </div>
+  );
+};
